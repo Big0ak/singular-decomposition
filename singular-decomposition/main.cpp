521fbe0cd47b1ac20ab8c7af7c49752e74ba7096
Maks <vrnmaksim@mail.ru>
2024-03-26 00:40:12 +0300

    add all cases and matrix operation

--- singular-decomposition/main.cpp
+++ singular-decomposition/main.cpp
@@ -2,7 +2,11 @@
 #include <string.h>
 #include <vector>
 #include <math.h>
-#include <ccomplex>
+#include <complex>
+
+#include "matrix.h"
+
+#define M_PI	3.14159265358979323846
 
 void print(std::vector<double> a, int n)
 {
@@ -14,7 +18,7 @@ void print(std::vector<double> a, int n)
 }
 
 // [0, x, x^2, x^3 ...]
-std::vector<double> multiply_polynomials(std::vector<double> a, std::vector<double> b, int n, int m)
+std::vector<double> multiply_polynomials(std::vector<double> a, int n, std::vector<double> b, int m)
 {
 	int size = m + n - 1;
 	std::vector<double> res;
@@ -32,6 +36,17 @@ std::vector<double> multiply_polynomials(std::vector<double> a, std::vector<doub
 	return res;
 }
 
+double Cube_root(double x)
+{
+	if (x > 0)
+	{
+		return std::pow(x, 1.0 / 3.0);
+	}
+	else {
+		return -std::pow(-x, 1.0 / 3.0);
+	}
+}
+
 void Cardano_real_number(std::vector<double> poly, std::vector<std::complex<double>>& answer)
 {
 	if (poly.size() != 4)
@@ -46,6 +61,11 @@ void Cardano_real_number(std::vector<double> poly, std::vector<std::complex<doub
 	}
 	poly[3] = 1;
 
+	/*
+	* kardano
+	* x^3 + px + q = 0;
+	*/
+
 	double p = poly[1] - std::pow(poly[2], 2) / 3.0;
 	double q = 2 * std::pow(poly[2], 3) / 27.0 - (poly[1] * poly[2]) / 3.0 + poly[0];
 
@@ -53,10 +73,11 @@ void Cardano_real_number(std::vector<double> poly, std::vector<std::complex<doub
 	if (D > 0)
 	{
 		// x0 - R, x1, x2 - C
-		double alpha = std::pow((-q / 2.0 + std::sqrt(D)), 1.0 / 3.0);
-		double beta = std::pow((-q / 2.0 - std::sqrt(D)), 1.0 / 3.0);
+		double alpha = Cube_root((-q / 2.0 + std::sqrt(D)));
+		double beta = Cube_root((-q / 2.0 - std::sqrt(D)));
 
 		answer[0].real(alpha + beta);
+
 		answer[1].real(-(alpha + beta) / 2.0);
 		answer[1].imag(std::sqrt(3) * ((alpha - beta) / 2));
 
@@ -67,22 +88,30 @@ void Cardano_real_number(std::vector<double> poly, std::vector<std::complex<doub
 	}
 	else if (D < 0)
 	{
+		double phi = std::atan(-2 * std::sqrtf(D) / q);
+		double r = 2 * std::sqrt(-p / 3);
 
+		answer[0].real(r * std::cos(phi / 3.0));
+		answer[0].imag(0.);
+
+		answer[1].real(r * std::cos(phi + 2 * M_PI / 3.0));
+		answer[1].imag(0.);
+
+		answer[2].real(r * std::cos(phi + 4 * M_PI / 3.0));
+		answer[2].imag(0.);
 	}
 	else {
-		double alpha = std::pow((-q / 2.0 + std::sqrt(D)), 1.0 / 3.0);
-		double beta = std::pow((-q / 2.0 - std::sqrt(D)), 1.0 / 3.0);
-
+		double alpha = Cube_root((-q / 2.0));
+	
 		answer[0].real(2.0 * alpha);
-		answer[0].imag(0);
+		answer[0].imag(0.);
 
 		answer[1].real(-alpha);
-		answer[1].imag(0);
+		answer[1].imag(0.);
 
 		answer[2].real(-alpha);
-		answer[2].imag(0);
+		answer[2].imag(0.);
 	}
-
 }
 
 int main()
@@ -100,23 +129,23 @@ int main()
 
 	m = { {1,1,3}, {1,5,1}, {3,1,1} };
 
+	//Matrix a_t(m);
+	//a_t.transposition();
+
+	//Matrix a(m);
+	//Matrix gram(a_t * a);
+
 	std::vector<double> lambda_poly = multiply_polynomials(
-		multiply_polynomials({ m[0][0], -1 }, { m[1][1], -1 }, 2, 2),
-		{ m[2][2], -1 },
+		multiply_polynomials({ m[0][0], -1 }, 2, { m[1][1], -1 }, 2),
 		3,
+		{ m[2][2], -1 },
 		2
 	);
 
 	lambda_poly[0] += m[1][0] * m[2][1] * m[0][2] + m[0][1] * m[1][2] * m[2][0];
 
-	/*
-	 * kardano
-	 * x^3 + px + q = 0;
-	 */
-
 	std::vector<std::complex<double>> answer;
 	Cardano_real_number(lambda_poly, answer);
-
 
 	return 0;
 }
--- singular-decomposition/matrix.h
+++ singular-decomposition/matrix.h
@@ -1,16 +1,54 @@
 #pragma once
 #include <vector>
 
-
 class Matrix
 {
 public:
 	Matrix() : cols(0), rows(0) {}
+	Matrix(std::vector<std::vector<double>> _matrix)
+	{
+		cols = _matrix.size();
+		rows = _matrix[0].size();
+		matrix = _matrix;
+	}
+	Matrix(int _cols, int _rows)
+	{
+		matrix.resize(_cols);
+		for (int i = 0; i < matrix.size(); i++)
+			matrix[i].resize(_rows);
+		cols = _cols;
+		rows = _rows;
+	}
+
 
 	~Matrix();
+
+	double& operator()(int i, int j) { return matrix[i][j]; }	// получение элемента
+	void operator()(int i, int j, double data) { matrix[i][j] = data; }   // установка элемента
+
+	void transposition()
+	{
+		std::vector<std::vector<double>> m_new;
+		m_new.resize(rows);
+		for (int i = 0; i < m_new.size(); i++)
+			m_new[i].resize(cols);
+		for (int i = 0; i < cols; i++)
+		{
+			for (int j = 0; j < rows; j++)
+			{
+				m_new[j][i] = matrix[i][j];
+			}
+		}
+		m_new = matrix;
+	}
+
+	Matrix& operator* (const Matrix _matrix)
+	{
+		
+	}
+
 	void multiplication();
 	void reverse();
-	void transposition();
 	void determinant();
 
 private:
